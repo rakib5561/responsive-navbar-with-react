4f519cc4e7d8800aaae75f336b6a710f82c6bf6b
Md. Rakib <mdrakib5561@gmail.com>
2022-01-05 22:50:29 +0600

    completed navbar with responsive

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,22 +1,40 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Navbar.css';
 
 const Navbar = () => {
 
     const [toggleMenu, setToggleMenu] = useState(false);
+    const [screenWidth, SetScreenWidth] = useState(window.innerWidth);
+
+    const togglenav = ()=>{
+        setToggleMenu(!toggleMenu);
+    }
+
+    useEffect(()=>{
+
+        const changeWidth = () =>{
+            SetScreenWidth(window.innerWidth);
+        }
+        window.addEventListener('resize', changeWidth)
+
+    },[])
 
     return (
         <div>
             
             <nav>
-                <ul className='menu-list'>
+                {
+                    (toggleMenu || screenWidth > 500)&&(
+                        <ul className='menu-list'>
                     <li className='items'>Home</li>
                     <li className='items'>About</li>
                     <li className='items'>Skills</li>
                     <li className='items'>Portfolio</li>
                     <li className='items'>Contact</li>
                 </ul>
-                <button className='toggole-btn'>click</button>
+                    )
+                }
+                <button onClick={togglenav} className='toggole-btn'>click</button>
             </nav>
 
         </div>
